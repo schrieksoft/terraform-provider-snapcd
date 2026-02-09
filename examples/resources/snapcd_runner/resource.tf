4cda1d87effbed6c934a19869b17f1745084e355
Karl Schriek <karl.schriek@snapcd.io>
2026-02-09 15:52:26 +0100

    small documentation fix

--- examples/resources/snapcd_runner/resource.tf
+++ examples/resources/snapcd_runner/resource.tf
@@ -5,5 +5,6 @@ data "snapcd_service_principal" "my_service_principal" {
 
 resource "snapcd_runner" "my_runner" {
   name                       = "MyGeneralRunner"
+  service_principal_id       = data.my_service_principal.runner.id
   is_assigned_to_all_modules = true // all modules from all stacks can use this Runner. If you want to restrict this, set to "false" and then use "snapcd_runner_stack_assignment", "snapcd_runner_namespace_assignment" or "snapcd_runner_module_assignment" to assign to a narrower scope.
 }
